2ff5b9d8c98f73a0faee0a90ba40c37fb9bff422
King <ivan_zukkel@mail.ru>
2022-03-05 10:17:21 +0300

    Небольшие корректировки

--- src/main/java/com/example/renderfarm/service/TaskService.java
+++ src/main/java/com/example/renderfarm/service/TaskService.java
@@ -20,6 +20,7 @@ import java.util.List;
 import java.util.Optional;
 
 @Service
+@EnableScheduling
 @RequiredArgsConstructor
 public class TaskService {
     private final PersonRepository personRepository;
